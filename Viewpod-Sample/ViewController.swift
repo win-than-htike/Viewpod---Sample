b16969eca5011ad4694e24fc50daf1520a82db79
winthanhtike <winnthannhtike@gmail.com>
2018-11-24 14:27:29 +0630

    Initial Commit

---
+++ Viewpod-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Viewpod-Sample
+//
+//  Created by Win Than Htike on 11/24/18.
+//  Copyright © 2018 PADC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
